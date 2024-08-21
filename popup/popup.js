97e46da6adad2213a8a70087b7429424ed5ec548
BADREDDINE <mrbadrox1997@gmail.com>
2024-08-21 21:41:28 +0100

    remove files

--- popup/popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('captureBtn').addEventListener('click', () => {
-  chrome.runtime.sendMessage({action: "takeScreenshot"});
-  window.close();
-});
--- screenshot.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "displayScreenshot") {
-    document.getElementById('screenshot').src = request.imageUrl;
-  }
-});
